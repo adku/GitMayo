7c4a922794dd857430ab9ebca3afe3b67a89e857
adku <adku@adku.pl>
2014-04-02 22:56:15 +0200

    Initial commit from local git

---
+++ GitMayo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitMayo
+//
+//  Created by Adrian Kubisztal on 02.04.2014.
+//  Copyright (c) 2014 adku. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitMayo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GitMayo
+//
+//  Created by Adrian Kubisztal on 02.04.2014.
+//  Copyright (c) 2014 adku. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
